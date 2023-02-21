15accb566854fe619c6b69c8b4023bac6b679f12
greg <grandersson1991@gmail.com>
2023-02-20 20:05:14 -0500

    submitting for step 64 requirements

---
+++ Basic Javascript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,26 @@
+let a = "this is a string"
+window.alert(a);
+
+let b = "this \"sentence\" has quotation marks"
+
+document.write(a + b);
+
+let x = 1, y = 2, z = 3;
+
+document.write(x);
+
+// step 58
+// this code is an expression
+let exp = 74 + 42;
+
+// step 63
+// this code is writing a function to return a string
+let returnString = () => "this code is just an example"
+console.log(returnString());
+
+
+// step 64 submission reqs
+// this code concatenates two strings and logs them to the console
+let sent1 = "This is the beginning of a string";
+let sent2 = "and this is the end of a string";
+console.log(sent1 + " " + sent2);
